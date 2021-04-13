c26e2c37cb9afc773ed574f6a834503e844a200b
dselarka <dselarka@iu.edu>
2021-04-12 22:48:38 -0400

    configured model for the application

--- bug_tracker/tracker/models.py
+++ bug_tracker/tracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Bug(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField()
+    team = models.CharField(max_Length=255)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date-added']   
