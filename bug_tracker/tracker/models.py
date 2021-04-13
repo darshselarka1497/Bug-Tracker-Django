c56faa0f1d1d22d50a9e45d37a54f58dcb7d26dd
dselarka <dselarka@iu.edu>
2021-04-12 22:54:26 -0400

    make migrations

--- bug_tracker/tracker/models.py
+++ bug_tracker/tracker/models.py
@@ -6,8 +6,8 @@ class Bug(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField()
-    team = models.CharField(max_Length=255)
+    team = models.CharField(max_length=255)
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-date-added']   
+        ordering = ['-date_added']   
