c6349d90c26052b21285fcdf870aff9f5ff65bd3
dselarka <dselarka@iu.edu>
2021-05-02 16:36:30 -0400

    updating files

--- bug_tracker/tracker/views.py
+++ bug_tracker/tracker/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import Bug
+
 # Create your views here.
+def frontpage(request):
+    bugs = Bug.objects.all()
+    return render(request, 'tracker/frontpage.html', {'bugs': bugs})
